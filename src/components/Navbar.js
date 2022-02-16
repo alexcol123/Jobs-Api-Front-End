779731a55b7d5268b2e8d239ae87560d0a41f819
Henry Munoz <alexcol123@yahoo.com>
2022-02-16 15:10:06 -0500

    added context api

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+ <h2>Navbar</h2>
+ <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore sint, quibusdam deleniti non ullam nesciunt! Corrupti neque ullam error laborum quas assumenda dolore molestias voluptas fuga. Consectetur hic magnam quis nihil ea nostrum reprehenderit quo mollitia, optio iure, inventore quos? Illum culpa provident explicabo distinctio necessitatibus? Odio impedit eveniet natus.</p>
+    </div>
   )
 }
 
